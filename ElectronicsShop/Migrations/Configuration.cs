490a69038fc52aa3fd245c7a588bee48e7c94223
Łukasz <lukaszk9396@gmail.com>
2020-04-24 10:38:07 +0200

    Initial Commit

---
+++ ElectronicsShop/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace ElectronicsShop.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<ElectronicsShop.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    } 
+}
